c07b7edfe9ac8e39b1f3b8fc89613de701b1354b
shashikant kumar <shashi94goswami@gmail.com>
2025-02-18 21:47:57 +0530

    added polyfill in js call bind apply

--- Advance-react/Theme/src/App.tsx
+++ Advance-react/Theme/src/App.tsx
@@ -1,25 +1,14 @@
-import useTheme  from "./hooks/useContext"
-import './App.css'
-
+// import useTheme from "./hooks/useContext";
+import "./App.css";
+import Header from './components/headers/index'
 const App: React.FC = () => {
-  const { theme, toggleTheme } = useTheme();
 
-  // Set CSS variables
-  document.documentElement.style.setProperty('--theme-background', theme.background);
-  document.documentElement.style.setProperty('--theme-color', theme.color);
-console.log(theme,"theme color")
   return (
     <div className="theme-container">
-      <h1>React Theme Switcher</h1>
-      <button 
-        type="button" 
-        className="theme-toggle-button" 
-        onClick={toggleTheme}
-      >
-        Toggle Theme
-      </button>
+      <Header/>
+      
     </div>
   );
 };
 
-export default App
+export default App;
---
+++ Advance-react/Theme/src/components/headers/index.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Header.css";
+
+const Header: React.FC = () => {
+  return (
+    <div className="Header">
+      <div></div>
+      <div>
+        <h3>Header</h3>
+      </div>
+      <div className="Icon">Icon</div>
+    </div>
+  );
+};
+
+export default Header;
